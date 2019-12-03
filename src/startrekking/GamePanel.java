f517145193f84e37d48e1a4c4255fb0c9453f72c
Carmine Napolano <napolanocarmine@gmail.com>
2019-12-03 16:34:32 +0100

    Initial Game Panel

---
+++ src/startrekking/GamePanel.java
@@ -0,0 +1,180 @@
+package startrekking;
+
+import java.awt.BorderLayout;
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author CARMINE
+ */
+public class GamePanel extends Canvas implements Runnable{
+    /*
+    Responsabilità:
+    aprire e disegnare il frame, thread di aggiornamento
+    
+    Operazioni:
+    1- update ciclico di Map e Entity
+    2- Riferimenti di oggetti Map, Entity, KeyHandler, TileFacade
+*/
+    
+    //dimensione finestra
+    public static final int WIDTH = 300;
+    public static final int HEIGHT = WIDTH / 12 * 9;
+    public static final int SCALE = 3;
+    
+    //nome JFrame
+    public static final String NAME = "STAR TREKKING";
+    
+    public static int oldFrameCount;
+    
+    private JFrame frame;
+    private Thread thread;
+    private boolean running = false;
+    
+    private BufferedImage img;
+    private Graphics2D g;
+    
+    private TileFacade tf;
+    
+    public KeyHandler key;
+
+    public GamePanel() {
+        //settiamo le dimensioni della finestra
+        setMinimumSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+        setMaximumSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+        setPreferredSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+        
+        frame = new JFrame(NAME);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+
+        frame.add(this, BorderLayout.CENTER);
+        frame.pack();
+
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+    
+    public void addNotify(){
+        super.addNotify();
+        if(thread == null){
+            thread = new Thread(this, "GameThread");
+            thread.start();
+        }
+    }
+    
+    public void init(){
+        
+        running = true;
+        
+        img = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_ARGB);
+        g = (Graphics2D) img.getGraphics();
+        
+        key = new KeyHandler(this);
+        
+        tf = new TileFacade();
+        
+    }
+
+    @Override
+    public void run() {
+        
+        init();
+        
+        final double GAME_HERTZ = 60.0;
+        final double TBU = 1000000000 / GAME_HERTZ;
+        
+        final int MUBR = 5;
+        
+        double lastUpdateTime = System.nanoTime();
+        double lastRenderTime;
+        
+        final double TARGET_FPS = 60;
+        final double TTBR = 1000000000 / TARGET_FPS;
+        
+        int frameCount = 0;
+        int lastSecondTime = (int) (lastUpdateTime / 1000000000);
+        oldFrameCount = 0;
+        
+        while(running){
+            
+            double now = System.nanoTime();
+            int updateCount = 0;
+            while(((now - lastUpdateTime) > TBU) && (updateCount < MUBR)){
+                update();
+                input(key);
+                lastUpdateTime += TBU;
+                updateCount++;
+            }
+            
+            if(now - lastUpdateTime > TBU){
+                lastUpdateTime = now - TBU;
+            }
+            
+            input(key);
+            
+            render();
+            draw();
+            
+            lastRenderTime = now;
+            frameCount++;
+            
+            int thisSecond = (int) (lastUpdateTime / 1000000000);
+            if(thisSecond > lastSecondTime){
+                if(frameCount != oldFrameCount){
+                    System.out.println("NEW SECOND " + thisSecond + " " + frameCount);
+                    oldFrameCount = frameCount;
+                }
+                frameCount = 0;
+                lastSecondTime = thisSecond;
+            }
+            
+            while(now - lastRenderTime < TTBR && now - lastUpdateTime < TBU){
+                Thread.yield();
+                
+                try{
+                    Thread.sleep(1);
+                }catch(Exception e){
+                    System.out.println("ERROR: yielding thread");
+                }
+                now = System.nanoTime();
+            }
+        }
+        
+    }
+    
+    public void update(){
+        tf.update();
+    }
+    
+    public void render(){
+        if(g != null){
+            g.setColor(new Color(66, 134, 244));
+            g.fillRect(0, 0, WIDTH, HEIGHT);
+            tf.render(g);
+        }
+    }
+    
+     public void draw(){
+        Graphics g2 = (Graphics) this.getGraphics();
+        g2.drawImage(img, 0, 0, WIDTH, HEIGHT, null);
+        g2.dispose();
+    }
+    
+    public static void main(String[] args){
+        new GamePanel();
+    }
+
+    private void input(KeyHandler key) {
+        tf.input(key);
+    }
+    
+    
+}
---
+++ src/startrekking/KeyHandler.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package startrekking;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+/**
+ *
+ * @author CARMINE
+ */
+public class KeyHandler implements KeyListener{
+
+    public KeyHandler() {
+    }
+    
+    public KeyHandler(GamePanel game){
+        game.addKeyListener(this);
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+    }
+    
+}

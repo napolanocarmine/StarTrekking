6a3f37f0dd4aaaaa6b8810df6df1474adcb705e4
emiso <emiso@172.19.191.19>
2019-12-03 17:58:23 +0100

    This is the enum for the Entity States

---
+++ src/util/EntityState.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+/**
+ *
+ * @author emiso
+ */
+
+public enum EntityState{
+    run, jump, crunch, attack, dead
+}
